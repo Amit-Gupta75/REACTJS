02dc48fddb45b9a4dbdbbd15e6a199889d1b8d56
Amit Gupta <guptaamit7571@gmai.com>
2026-02-02 19:25:43 +0530

    create custom react

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -3,8 +3,16 @@ import { createRoot } from 'react-dom/client'
 
 import App from './App.jsx'
 
+function MyApp(){
+    return(
+        <div>Custom App !</div>
+    )
+}
+
 createRoot(document.getElementById('root')).render(
- 
-    <App />
+ <>
+   <App />
+  <MyApp/>
+ </> 
   
 )
